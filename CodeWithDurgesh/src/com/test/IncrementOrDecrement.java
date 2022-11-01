30470f92596c8bc686c22ba3bdadf11bbc2c75ff
akshada bangar <akshada bangar@LAPTOP-FK0NLA2C>
2022-11-01 22:13:23 +0530

    VERSION1

---
+++ CodeWithDurgesh/src/com/test/IncrementOrDecrement.java
@@ -0,0 +1,17 @@
+package com.test;
+
+public class IncrementOrDecrement {
+
+	public static void main(String[] args) {
+		int a=100;
+		int b=200;
+		a++;//INCREMENT
+		b--;//DECREMENT
+		System.out.println("a++ is:"+a);
+        System.out.println("b-- is:"+b);
+	}
+
+}
+/*OUTPUT-
+a++ is:101
+b-- is:199*/
