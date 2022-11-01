bfff036d7378a26226d95173ffad0c1b85830770
akshada bangar <akshada bangar@LAPTOP-FK0NLA2C>
2022-11-01 22:04:10 +0530

    version2

--- CodeWithDurgesh/src/com/test/TernaryOperator.java
+++ CodeWithDurgesh/src/com/test/TernaryOperator.java
@@ -1,19 +1,19 @@
 package com.test;
 /* Ternary operator= (? :)
-variable = (condition) ? expression1 : expression2 ...
-MEANS CONDIOIN JAR TRUE ASEL TAR EXPRESSION1 PRINT HONAR ANI CONDITION JAR FALSE ASEL TAR EXPRESSION2 PRONT HONAR...*/
+variable = (condition) ? value of True : value of False ...
+MEANS CONDIOIN JAR TRUE ASEL TAR True PRINT HONAR ANI CONDITION JAR FALSE ASEL TAR False PRONT HONAR...*/
 
 public class TernaryOperator 
 {
 
 public static void main(String[] args) 
 {
-	int x, y;  
-	x = 20;  
-	y = (x == 1) ? 61: 90;  //FALSE (20==1)
+	int x = 20;  
+	int y;
+	y = (x == 1) ? 100: 200;  //FALSE (20==1)
 	System.out.println("Value of y is: " +  y);  
 	
-	y = (x == 20) ? 61: 90;  //TRUE(20=20)
+	y = (x == 20) ? 100: 200;  //TRUE(20=20)
 	System.out.println("Value of y is: " + y);  
 	}  
 	}  
