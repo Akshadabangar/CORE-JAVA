14602ecdf716f7a1b6c3e1d4c24308691ededb23
akshada bangar <akshada bangar@LAPTOP-FK0NLA2C>
2022-11-01 23:02:36 +0530

    VERSION1

---
+++ CodeWithDurgesh/src/com/test/Threevariablegreater.java
@@ -0,0 +1,25 @@
+package com.test;
+
+public class Threevariablegreater {
+
+	public static void main(String[] args)
+	{
+		int a=10;
+		int b=3;
+		int c=7;
+		if(a>b && a>c)
+		{
+			System.out.println("a is Greater");
+		}
+		else if(b>a && b<c)
+		{
+			System.out.println("b is Greater");
+		}
+		else if(c>a &&c>b)
+		{
+			System.out.println("c is Freater");
+		}
+
+	}
+
+}
