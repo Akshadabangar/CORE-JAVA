5a4d0e43b0ef571daab0e75dad6fb5a6da5e619d
akshada bangar <akshada bangar@LAPTOP-FK0NLA2C>
2022-11-02 16:48:54 +0530

    VERSION1

---
+++ CodeWithDurgesh/src/dowhile/Fibonacci1.java
@@ -0,0 +1,32 @@
+package dowhile;
+// DO WHILE CHECK THE CONDITION AFTER EXECUTION BLOCK...
+
+import java.util.Scanner;
+public class Fibonacci1 {
+
+	public static void main(String[] args) 
+	{
+		int a=0,b=1,c,i=1;
+		Scanner sc=new Scanner(System.in);
+       System.out.println("Enter the range:");
+       int range=sc.nextInt();
+       
+       while(i<=range)
+       {
+    	   System.out.println(a+" ");
+    	   c=a+b;
+    	   a=b;
+    	   b=c;
+    	   i++;
+       }
+	}
+
+}
+/*OUTPUT=Enter the range:
+6
+0 
+1 
+1 
+2 
+3 
+5 */
